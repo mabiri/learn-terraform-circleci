4aecaef4a2e4828308b7ab6e5f79aeb1c8da42bb
Mark <mark@artelliga.com>
2021-09-21 14:04:09 -0700

    prettified

--- main.tf
+++ main.tf
@@ -7,11 +7,11 @@ terraform {
   }
   required_version = "> 0.14"
 
-backend "s3" {
+  backend "s3" {
     bucket = "circle-ci-backend-20210921204853129000000001"
     key    = "terraform/webapp/terraform.tfstate"
     region = "us-east-1"
-}
+  }
 
 }
 
